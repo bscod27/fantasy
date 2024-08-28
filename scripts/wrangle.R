736d84c4d07b154c02f7617060e80fc15c97be22
Bruno T. Scodari <bruno.scodari.gr@dartmouth.edu>
2024-08-28 17:17:05 -0400

    Renamed columns and reproduced data for modeling

--- scripts/wrangle.R
+++ scripts/wrangle.R
@@ -13,16 +13,16 @@ if (length(maxyear) == 0) {
 ##### 01. Read in data and wrangle ##### 
 filename <- list.files(path = '../data', pattern = 'data_.*')
 
-df <- read.csv(paste0('../data/', filename)) %>% 
+dat <- read.csv(paste0('../data/', filename)) %>% 
   rename_all(~c(
     'rk_ovr','player','team','pos',
-    'age','desc_games','desc_starts',
+    'age','desc_games','starts',
     'pass_comp','pass_att','pass_yds','pass_td','pass_int',
     'rush_att','rush_yds','y_a','rush_td',
     'rec_tgt','rec_rec','rec_yds','y_r','rec_td',
     'fumb_times','fumb_fl',
     'score_tds','score_2pm','score_2pp',
-    'fantpt','ppr','dkpt','fdpt','vbd',
+    'fantpt','ppr','dkpt','fdpt','rk_vbd',
     'rk_pos','ovrk',
     'year', 'rk_probowl', 'rk_allpro'
     )
@@ -32,35 +32,38 @@ df <- read.csv(paste0('../data/', filename)) %>%
     pos = ifelse(pos == 'FB', 'RB', pos), 
     ppg = (fantpt + (0.5*rec_rec)) / desc_games,
     ) %>% 
-  mutate_if(is.numeric, ~replace_na(., 999)) # this is an important line, coding missing vars to 999
+  mutate_if(is.numeric, ~replace_na(., 0)) # this is an important line, coding missing vars to 0
 
 # one hot encode positions 
-onehot_pos <- data.frame(model.matrix(~df$pos + 0))
+onehot_pos <- data.frame(model.matrix(~ dat$pos + 0)) 
 colnames(onehot_pos) <- substr(colnames(onehot_pos), 4, 1000)
 
 # select features we want
-df <- df %>% 
+df <- dat %>%
   select(-pos) %>%
   cbind(onehot_pos) %>% 
   select(
-    player, team, ppg, year, age, 
-    matches('^pos|^rk_|^pass_|^rush_|^rec_|^fumb_|^score_')
-  )
+    player, team, ppg, year, age, starts,
+    matches('^pos|^rk_|^y_|^pass_|^rush_|^rec_|^fumb_|^score_')
+  ) 
 
 
 ##### 02. Create newdat and modeldat dataframes #####
 newdat <- df %>% 
-  mutate(lag_ppg = ppg) %>% 
   filter(year == maxyear) %>% 
   select(-year)
+idx <- which(!colnames(newdat) %in% c('player', 'team'))
+colnames(newdat)[idx] <- paste0('lag_', colnames(newdat)[idx])
 
 traindat <- df %>% 
   group_by(player) %>%
   arrange(player, year) %>%
   mutate(lag_ppg = lag(ppg)) %>%
-  mutate_at(vars(matches('^pos|^rk_|^pass_|^rush_|^rec_|^fumb_|^score_')), lag) %>%
+  mutate_at(vars(matches('^pos|^rk_|^y_|^pass_|^rush_|^rec_|^fumb_|^score_')), lag) %>%
   filter(!is.na(lag_ppg)) %>%
   select(-year)
+idx <- which(!colnames(traindat) %in% c('player', 'team', 'lag_ppg', 'ppg'))
+colnames(traindat)[idx] <- paste0('lag_', colnames(traindat)[idx])
 
 
 ##### 03. Write out newdat and traindat to data #####
